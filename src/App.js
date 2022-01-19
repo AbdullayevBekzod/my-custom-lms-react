d5c78ddf02e833902bdda268a29afdb167e128eb
AbdullayevBekzod <abdullayev.bekzod.0403@gmail.com>
2022-01-19 21:18:40 +0500

    #1 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './globalComponents/Header/Header';
+import Footer from './globalComponents/Footer/Footer';
+import Dashboard from './globalComponents/Content/Dashboard/Dashboard';
+import LeftSidebar from './globalComponents/LeftSidebar/LeftSidebar';
+import Deadlines from './globalComponents/Content/Deadlines/Deadlines';
+import LessonsTable from './globalComponents/Content/LessonsTable/LessonsTable';
+import Grades from './globalComponents/Content/Grades/Grades';
+import Sharing from './globalComponents/Content/Sharing/Sharing';
+import Messenger from './globalComponents/Content/MessageContent/Messenger';
 
-function App() {
+const App = () => {
+  const isAuth = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="container-2xl h-screen">
+        <div className='grid grid-cols-12'>
+          <div className='col-span-12 p-3 bg-green-300'>
+            <Header isAuth={isAuth} />
+          </div>
+        </div>
+        <div className="grid grid-cols-12">
+          <div className='col-span-2 bg-blue-500 '>  
+            <LeftSidebar />
+          </div>
+            <div className='col-span-10'>
+              <Routes>
+                <Route path = '/' element={<Dashboard />}/>
+                <Route path = '/deadline' element={<Deadlines />}/>
+                <Route path = '/grades' element={<Grades />}/>
+                <Route path = '/lesson-table' element={<LessonsTable />}/>
+                <Route path = '/sharing' element={<Sharing />}/>
+                <Route path = '/messenger' element={<Messenger />}/>
+              </Routes>
+            </div>
+        </div>
+        <Footer />
+      </div>
+    </Fragment>
   );
 }
 
